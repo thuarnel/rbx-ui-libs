d17de06f99efaa327f434e7d85d7bedf5ba5a354
thuarnel <196966936+thuarnel@users.noreply.github.com>
2025-03-12 03:09:51 -0700

    lo fix (1/2)

--- Notifications/Lo/example.lua
+++ Notifications/Lo/example.lua
@@ -1,4 +1,4 @@
-local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/lobox920/Notification-Library/Main/Library.lua"))()
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Notification-Library/main/Library.lua"))()
 NotificationLibrary:SendNotification("Success", "I'm a cool message", 3)
 NotificationLibrary:SendNotification("Warning", "I'm a cool message", 3)
 NotificationLibrary:SendNotification("Error", "I'm a cool message", 3)
